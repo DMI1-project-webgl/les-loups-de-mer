b37953287818c228507093474b208346f514795f
Eliiiise <balvalloire@gmail.com>
2022-05-18 11:27:52 +0200

    :sparkles: style nav

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import Slide from './components/Slide.vue';
 
 <template>
   <!-- ALL ROUTES -->
-  <!-- <div class="nav">
+  <div class="nav">
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
     <router-link to="/clean">Clean</router-link>
@@ -20,7 +20,7 @@ import Slide from './components/Slide.vue';
     <router-link to="/result">Result</router-link>
     <router-link to="/shop">Shop</router-link>
     <router-link to="/newsletter">Newsletter</router-link>
-  </div> -->
+  </div>
 
   <!-- 3D COMPONENT  -->
 
@@ -43,6 +43,10 @@ header {
 
 .nav {
   z-index: 10;
+  position: fixed;
+  top: 20px;
+  left: 100px;
+  font-size: 15px;
 }
 
 .nav a {
