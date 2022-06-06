2cfeb0f4c8a8df18c56ccf7f648d4c5425076b63
Eliiiise <balvalloire@gmail.com>
2022-06-06 15:59:11 +0200

    :sparkles: button sound

--- src/components/AudioController.vue
+++ src/components/AudioController.vue
@@ -8,6 +8,11 @@
       <audio ref="successFinal" src="./../../src/assets/sound/reussite-finale.wav" preload="metadata" type="audio/wav"></audio>
 
       <audio ref="piano" src="./../../src/assets/sound/melodie_piano.wav" preload="metadata" autoplay loop type="audio/wav"></audio>
+
+      <div ref="audioControl" class="audio-control audio-control--disable" @click="toggleSound">
+          <img class="volume" ref="volume" src="../assets/img/volume.png">
+          <img class="mute" ref="mute" src="../assets/img/mute.png">
+      </div>
     </div>
 </template>
 
@@ -17,7 +22,8 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'AudioController',
   data: () => ({
-      loop: {current: 0, target: 0, coef: 0.005}
+      loop: {current: 0, target: 0, coef: 0.005},
+      soundStatus: true
   }),
   mounted () {
     this.signal.add(this.onSignal.bind(this));
@@ -64,19 +70,27 @@ export default defineComponent({
                 }, 4000)
             break
             case "experience-start":
-                this.loop.target = 0.6
+                this.loop.target = 0.6;
+                (this.$refs.piano as HTMLAudioElement).pause()
                 if (!(this.$refs.piano as HTMLAudioElement).paused) {
                     this.loopPlay()
                 }
                 break
             case "experience-end":
                 this.loop.target = -0.09
-                this.loop.coef = 0.1
+                this.loop.coef = 0.1;
+                (this.$refs.audioControl as HTMLElement).classList.add('audio-control--disable')
                 this.update()
         }
     },
     loopPlay() {
+        console.log('yoooooo', (this.$refs.piano as HTMLAudioElement).paused)
+        if (!(this.$refs.piano as HTMLAudioElement).paused) return
+        console.log('yooooooaaa', (this.$refs.piano as HTMLAudioElement).paused);
         (this.$refs.piano as HTMLAudioElement).play()
+        if (this.loop.target = 0.6) {
+            (this.$refs.audioControl as HTMLElement).classList.remove('audio-control--disable');
+        }
         this.loop.coef = 0.005
         this.update()
     },
@@ -91,9 +105,55 @@ export default defineComponent({
         this.loop.current = Math.max(this.lerp(this.loop.current, this.loop.target, this.loop.coef), 0);
         (this.$refs.piano as HTMLAudioElement).volume = this.loop.current
         setTimeout(this.update, 10)
+    },
+    toggleSound() {
+        if (this.soundStatus) {
+            this.soundStatus = false;
+            (this.$refs.volume as HTMLElement).style.display = 'block';
+            (this.$refs.mute as HTMLElement).style.display = 'none';
+            this.loop.target = -0.09
+            this.loop.coef = 0.1
+            this.update()
+        } else {
+            this.soundStatus = true;
+            (this.$refs.volume as HTMLElement).style.display = 'none';
+            (this.$refs.mute as HTMLElement).style.display = 'block';
+            this.loop.coef = 0.01
+            this.loop.target = 0.6
+            this.update()
+        }
     }
   },
   beforeDestroy () {
   }
 })
 </script>
+
+<style scoped>
+.audio-control {
+    position: fixed;
+    right: 0;
+    top: 0;
+    z-index: 5;
+    width: 50px;
+    height: 50px;
+    background-color: #fff;
+    border-radius: 50%;
+    padding: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 30px;
+    transition: opacity 0.4s ease-out;
+    cursor: pointer;
+}
+
+.audio-control--disable {
+    pointer-events: none;
+    opacity: 0;
+}
+
+.volume {
+    display: none;
+}
+</style>
